0aa5bf8ff06712748bb367b9d11cceacc092d825
ieduser <ieduser@fi-lab10-02.ied.edu>
2019-02-04 12:19:25 +0100

    Added if and else

--- Script.js
+++ Script.js
@@ -1,1 +1,16 @@
-prompt('Write a number:');
+//test
+
+var myNumber = prompt('Write a number:');
+myNumber = parseInt(myNumber);
+
+if(myNumber < 0 ) {
+    alert('Given number is negative. Absing it..');
+    myNumber = Math.abs(myNumber);
+}
+else{
+    alert('Given number is positive!')
+}
+
+var squareRoot = Math.sqrt(myNumber);
+
+alert(squareRoot);
