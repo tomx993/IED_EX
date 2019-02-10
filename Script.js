39eda99071d1e43ee1b856bb37ef426632d63d0e
Tommaso Dati <tommy@mbp-di-tommaso.homenet.telecomitalia.it>
2019-02-10 11:21:58 +0100

    esercizi da casa

--- Script.js
+++ Script.js
@@ -1,5 +1,7 @@
 //test
 
+/* 
+
 var myNumber = prompt('Write a number:');
 myNumber = parseInt(myNumber);
 
@@ -14,3 +16,38 @@ else{
 var squareRoot = Math.sqrt(myNumber);
 
 alert(squareRoot);
+
+*/
+
+var myNumber = prompt('Write a number:');
+myNumber = parseInt(myNumber);
+
+if(myNumber == 10 ) {
+    alert('Great!');
+}
+else{
+    alert('Bad! :(')
+}
+
+var myNumber2 = prompt('Write a second number:');
+myNumber2 = parseInt(myNumber2);
+
+if( myNumber2 == myNumber ){
+    alert('Twice? Great!!');
+}
+else{
+    alert('Bad! :(');
+}
+
+var string = prompt('Write a text:');
+
+alert('Another one!');
+
+var string2 = prompt('Write a second text:');
+
+if( string == string2 ){
+    alert('You are a badass!!');
+}
+else{
+    alert('Wrong man! :(');
+}
