426fa5527f66842e33c5dad563b4835715b8e695
ieduser <ieduser@fi-lab10-02.ied.edu>
2019-02-11 10:30:36 +0100

    Aggiornamento lezioni

---
+++ Lezione 2/Script.js
@@ -0,0 +1,73 @@
+//test
+
+/* 
+var myNumber = prompt('Write a number:');
+myNumber = parseInt(myNumber);
+
+if(myNumber < 0 ) {
+    alert('Given number is negative. Absing it..');
+    myNumber = Math.abs(myNumber);
+}
+else{
+    alert('Given number is positive!')
+}
+
+var squareRoot = Math.sqrt(myNumber);
+
+alert(squareRoot);
+*/
+
+// Esercizio 1
+function Excersize1() {
+    var myNumber = prompt('Write a number:');
+    myNumber = parseInt(myNumber);
+
+    if(myNumber == 10 ) {
+        alert('Great!');
+    }
+    else{
+        alert('Bad! :(');
+    }
+}
+// Esercizio 2
+function Excersize2() {
+    var myNumber2 = prompt('Write a second number:');
+    myNumber2 = parseInt(myNumber2);
+
+    if( myNumber2 == myNumber ){
+        alert('Twice? Great!!');
+    }
+    else{
+        alert('Bad! :(');
+    }
+}
+
+//Esercizio 3
+function Excersize3() {
+    var string = prompt('Write a text:');
+
+    alert('Another one!');
+
+    var string2 = prompt('Write a second text:');
+
+    if( string == string2 ){
+        alert('You are a badass!!');
+    }
+    else{
+        alert('Wrong man! :(');
+    }
+}
+//Esercizio 4
+function Excersize4() {
+    var word = prompt('Write a word');
+
+    if( word == 'pizza' || word == 'snack'){
+        alert('Pausa veloce');
+    }
+    else if( word == 'spaghetti' ){
+        alert('Pausa lunga');
+    }
+    else{
+        alert('Pausa indefinita');
+    }
+}
