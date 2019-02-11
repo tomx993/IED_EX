df73ba01f926e80fb31eae72f1f76e6988bb67dc
ieduser <ieduser@fi-lab10-02.ied.edu>
2019-02-11 11:26:04 +0100

    Const

---
+++ Lezione 3/java.js
@@ -0,0 +1,9 @@
+// IF
+
+      //sempre maiuscolo + underscore
+const COMPARABLE_NUMBER = 10;
+var readNumber = prompt('Scrivi un numero:');
+
+if( readNumber == COMPARABLE_NUMBER ){
+    alert("PARAGONE OK");
+}
