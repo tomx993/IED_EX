7c4c2406a07a6e94dbe23f6825ad2f7d72773b26
TOMMASO DATI <tommaso.dati@ied.edu>
2019-02-18 11:25:32 +0100

    For each

--- Lezione 4/script.js
+++ Lezione 4/script.js
@@ -2,9 +2,17 @@ const MILK_FOR_A_PORTION_ML = 60;
 
 function printMilkByPieEaters( peopleNumber ) {
     var milkTotal = peopleNumber * MILK_FOR_A_PORTION_ML;
-    console.log('Il latte necessario per '+peopleNumber+' persone corrisoponde a '+milkTotal+' ml');
+    console.log('Il latte necessario per '+peopleNumber+' persone corrisoponde a '+milkTotal+' ml.');
 }
 
 for( var peopleNumber = 1; peopleNumber <= 10; peopleNumber++ ){
     printMilkByPieEaters(peopleNumber);
+}
+
+// for each
+
+var numbers = [34, 44, 55, 76]
+
+for( var myNumbers in numbers ){
+    console.log( myNumbers + ' --> ' + numbers[myNumbers]);
 }
